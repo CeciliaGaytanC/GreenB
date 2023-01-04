0d6d91c7761ab7ce926a564eaa0fa58a1e5a2b01
CeciliaGaytan <cecilia.gaytc@gmail.com>
2023-01-04 00:24:19 -0600

    Se afrefo diseño de Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,175 @@
 import 'package:flutter/material.dart';
+import 'package:greenb/register.dart';
+
+import 'Boton.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
+  static const String _title = "";
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-       
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner:  false,
+
+      title: 'Prueba',
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 29, 30, 30),
+        body: const MyHomePage(),
+      
+      )
+  
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+    const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  //GlobalKey _formKey  = new GlobalKey<FormState>();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+    return Form(
+      key: _formKey,
+    
+            child: ListView(
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Container(
+              alignment: Alignment.center,
+              child: Text(
+                'CookApp',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.white,
+                ),
+              ),
+              padding: const EdgeInsets.all(46),
+              margin: EdgeInsets.all(28),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                shape: BoxShape.circle,
+              ),
+              width: 120,
+              height: 110,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+           
+            Padding(
+              // key: _formKey,
+              padding: const EdgeInsets.all(10),
+              child: TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Por favor, Ingresa el nombre de usuario';
+                  }
+                  return null;
+                },
+                style: TextStyle(color: Color.fromARGB(255, 15, 15, 15)),
+                controller: nameController,
+                decoration: const InputDecoration(
+                  // border: OutlineInputBorder(),
+                  //  labelText: 'Nombre de Usuario',
+                  hintText: "Ingrese su Nombre",
+                  prefixIcon: const Icon(
+                    Icons.person,
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child: TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Por favor, Ingresa tu contraseña';
+                  }
+                  return null;
+                },
+                style: TextStyle(color: Color.fromARGB(255, 19, 18, 18)),
+                obscureText: true,
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  //border: OutlineInputBorder(),
+                  //labelText: 'Contraseña',
+                  hintText: "Ingrese su Contraseña",
+                  prefixIcon: Icon(
+                    Icons.lock,
+                    color: Colors.blue,
+                  ),
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                'Olvidaste tu contraseña',
+              ),
+            ),
+            Container(
+                height: 50,
+                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: ElevatedButton(
+                  child: const Text('Iniciar Sesión'),
+                  onPressed: () {
+                    print(nameController.text);
+                    print(passwordController.text);
+                    if (_formKey.currentState!.validate()) {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Botonazo()));
+                      // Si el formulario es válido, queremos mostrar un Snackbar
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text(
+                            'Por favor, Ingresa los datos. Hay campos vacios'),
+                        backgroundColor: Colors.green,
+                        //width: 600,
+                        shape: StadiumBorder(),
+                        elevation: 560,
+                        //   comportamiento: SnackBarBehavior.floating;
+                      ));
+                    }
+                  },
+                )),
+            Row(
+              children: <Widget>[
+                const Text('Aun no tienes cuenta?'),
+                TextButton(
+                    child: const Text(
+                      'Registrate',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) {
+                          return Registro();
+                        }),
+                      );
+                    })
+              ],
+              mainAxisAlignment: MainAxisAlignment.center,
             ),
           ],
-        ),
+       
+   
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+  // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
